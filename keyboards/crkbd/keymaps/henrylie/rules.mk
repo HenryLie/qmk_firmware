de4bff22bad1ed9e1645bc09c3753dca928bbff5
Henry Roes Lie <henry.lie@outlook.com>
2022-01-25 01:45:45 +0900

    fix: use via keymap as base

--- keyboards/crkbd/keymaps/henrylie/rules.mk
+++ keyboards/crkbd/keymaps/henrylie/rules.mk
@@ -1,10 +1,7 @@
-
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = yes    # Enable VIA
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
